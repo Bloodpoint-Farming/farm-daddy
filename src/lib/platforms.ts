a1482e4be0223dc5c07e2818312a012004f4755d
snoggles <191955932+snoggles@users.noreply.github.com>
2025-12-13 08:44:15 -0500

    Add XBox One

--- src/lib/platforms.ts
+++ src/lib/platforms.ts
@@ -2,7 +2,8 @@ export const PLATFORMS = {
     steam: { label: 'Steam', short: 'Steam' },
     epic: { label: 'Epic Games', short: 'Epic' },
     windows: { label: 'Windows Store', short: 'Win' },
-    xbox: { label: 'Xbox', short: 'Xbox' },
+    xboxOne: { label: 'Xbox One', short: 'XbOne' },
+    xbox: { label: 'Xbox X/S', short: 'Xbox' },
     ps4: { label: 'PlayStation 4', short: 'PS4' },
     ps5: { label: 'PlayStation 5', short: 'PS5' },
     switch: { label: 'Nintendo Switch', short: 'Switch' }
